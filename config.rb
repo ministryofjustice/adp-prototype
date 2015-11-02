ff91abb06b11b59a7c84cac0f5153bbd53ae3a85
Tyom Semonov <inspiro@gmail.com>
2015-11-02 11:56:05 +0000

    Fix relative paths

--- config.rb
+++ config.rb
@@ -129,6 +129,8 @@ end
 
 # Build-specific configuration
 configure :build do
+  set :relative_links, true
+
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
@@ -139,7 +141,7 @@ configure :build do
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
